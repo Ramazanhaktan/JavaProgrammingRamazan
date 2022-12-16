93a8a47565cd5ee12318cb656e9c3b524fd6199e
Ramazan Haktan Helvacı <rmznhktnhlvc@gmail.com>
2022-12-16 08:23:43 +0100

    day39: Recap

--- src/examples/Test.java
+++ src/examples/Test.java
@@ -3,16 +3,68 @@ package examples;
 
 public class Test {
 
-public void Constructor(){
-    System.out.println("sas");
-}
+    public void Constructor() {
 
-
-
-    public static void main(String[] args) {
-        Test obj = new Test();
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
